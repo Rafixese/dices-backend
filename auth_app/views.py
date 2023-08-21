82f825614fa9538b40f6222881cf8ed57ce90e08
Rafał Hrabia <rafixese@gmail.com>
2023-08-21 21:55:10 +0200

    Added auth app

---
+++ auth_app/views.py
@@ -0,0 +1,19 @@
+from django.contrib.auth.models import User
+from rest_framework import viewsets
+from rest_framework.permissions import IsAuthenticated, IsAdminUser
+
+from auth_app.serializers import UserSerializer
+
+
+class UserViewSet(viewsets.ModelViewSet):
+    queryset = User.objects.all()
+    serializer_class = UserSerializer
+
+    def get_permissions(self):
+        if self.action == 'destroy':
+            permission_classes = [IsAuthenticated, IsAdminUser]
+        elif self.action == 'update':
+            permission_classes = [IsAuthenticated, IsAdminUser]
+        else:
+            permission_classes = []
+        return [permission() for permission in permission_classes]
